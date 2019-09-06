c2c84d367528a91ec56c41d1a6ab334bb92e0961
Joe Chasinga <jo.chasinga@gmail.com>
2019-09-05 18:04:35 -0700

    Init repo

---
+++ main.jl
@@ -0,0 +1,91 @@
+using DataStructures
+using Formatting
+
+most_common(c::Accumulator) = most_common(c, length(c))
+most_common(c::Accumulator, k) = sort(collect(c), by=kv->kv[2], rev=true)[1:k]
+
+function isalpha(str)
+    re = r"^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$"
+    !occursin(re, str)
+end
+
+"Make a bag of words from email text."
+function make_dictionary(root_dir)
+    all_words = []
+    emails = [joinpath(root_dir, f) for f in readdir(root_dir)]
+    for (i, mail) in enumerate(emails)
+        println("Opening file: ", i, " ", mail)
+        open(mail) do m
+            for line in readlines(m)
+                words = split(line)
+                append!(all_words, words)
+            end
+        end
+    end
+
+    bag = counter(all_words)
+    list_to_remove = [k for k in keys(bag)]
+
+    for item in list_to_remove
+        # remove if numerical
+        if !isalpha(item)
+            reset!(bag, item)
+            # pop!(bag, item)
+        elseif length(item) == 1
+            reset!(bag, item)
+            # pop!(bag, item)
+        end
+    end
+    # Consider only most 3000 common words
+    most_common(bag, 3000)
+end
+
+"Make features matrix and label vector"
+function extract_features(mail_dir, dict)
+    files = [joinpath(mail_dir, f) for f in readdir(mail_dir)]
+    features_matrix = zeros(length(files), 3000)
+    train_labels = zeros(length(files))
+    # cnt = 1
+    doc_id = 1
+    for (n, file) in enumerate(files)
+        open(file) do f
+            for (i, line) in enumerate(readlines(f))
+                # Skip the first subject line and second empty line
+                if i == 3
+                    words = split(line)
+                    for word in words
+                        word_id = 1
+                        # Go through the bag of words
+                        for (i, d) in enumerate(dict)
+                            if d[1] == word
+                                printfmt("Found {} in the bag\n", word)
+                                word_id = i
+                                c = count(x -> x == word, words)
+                                features_matrix[doc_id, word_id] = c
+                                printfmt("F[{}, {}] = {}\n", doc_id, word_id, c)
+                            end
+                        end
+                    end
+                end
+            end
+
+            train_labels[doc_id] = 0
+            filepath_tokens = split(file, "/")
+            last_token = filepath_tokens[length(filepath_tokens)]
+            if startswith(last_token, "spmsg")
+                train_labels[doc_id] = 1
+                # TODO: ?
+                # cnt += 1
+            end
+            doc_id += 1
+        end
+    end
+    (features_matrix, train_labels)
+end
+
+
+
+d = make_dictionary("machine-learning-101/chapter1/train-mails")
+(f, t) = extract_features("./machine-learning-101/chapter1/train-mails", d)
+
+# println(f)
